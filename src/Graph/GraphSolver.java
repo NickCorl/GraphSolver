76eba542cabf0820aee3f5e75a71bba880858404
NickCorl <nook.corl@gmail.com>
2015-04-11 20:57:11 -0600

    got code to run through map but there is error in pathFinder

--- src/Graph/GraphSolver.java
+++ src/Graph/GraphSolver.java
@@ -7,13 +7,15 @@ import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class GraphSolver {
 	
-	public static String fileName = "grandpaTransitInput";
+	private String fileName = "grandpaTransitInput";
 	private int townNumber;
 	private int pathNumber;
-	public static Map<Character, Queue<String>> paths = new HashMap<Character, Queue<String>>();
+	private Stack<String> desiredPath = new Stack<String>();
+	public Map<Character, Queue<String>> paths = new HashMap<Character, Queue<String>>();
 
 	private void fileOpener(String Filename){
 		try {
@@ -62,8 +64,68 @@ public class GraphSolver {
 	
 	public static void main(String[] args) {
 		GraphSolver gs = new GraphSolver();
-		gs.fileOpener(fileName);
+		//RouteFinder rf = new RouteFinder();
+		
+		Stack<String> answer = new Stack<String>();
+		
+		gs.fileOpener(gs.fileName);
+		gs.desiredPath.push(gs.paths.get('A').peek());
+		answer = gs.pathFinder(gs.paths.get('A').remove());
+		
+		for (String way : answer){
+			System.out.println(way);
+		}
 
+	}
+
+	public Stack<String> pathFinder(String direction){
+		Queue<String> path = new PriorityQueue(); // these will store the looked at strings and return them to the queue on the map.
+		char[] currentPath = direction.toCharArray();
+		char[] comparePath = paths.get(currentPath[1]).peek().toCharArray();
+
+		System.out.println(direction + "," + desiredPath.size());///////////////////////////
+		if (currentPath[1] == 'i'){
+			return desiredPath;
+		}
+		
+		//else if (desiredPath.contains(direction)){
+			//System.out.println("1");///////////////////////////////////////////
+			//desiredPath.pop();
+			
+		//	path = paths.get(currentPath[0]);
+			//path.remove();
+			
+		//	paths.put(currentPath[0], new PriorityQueue());
+		//	paths.put(currentPath[0], path);
+			
+		//	return pathFinder(desiredPath.peek());
+		//}
+		
+		else if( paths.get(currentPath[1]).isEmpty()){
+			System.out.println("2");///////////////////////////////////////////////////////////////////////
+			desiredPath.pop();
+			return (pathFinder(desiredPath.peek()));
+		}
+		
+		else if( currentPath[2] == comparePath[2] || currentPath[3] == comparePath[3] ){
+			System.out.println("3"+ "," + desiredPath.size());///////////////////////////////////////////////////////////
+			desiredPath.push(direction);
+			return pathFinder(paths.get(currentPath[0]).remove());
+		}
+		
+		else if( currentPath[2] != comparePath[2] && currentPath[3] != comparePath[3] ){
+			System.out.println( paths.get(currentPath[1]).peek());///////////////////////////////////////////////////
+			
+			for (String searched : paths.get(currentPath[1])){
+				System.out.println("4");//////////////////////////////////////////////////////////
+				System.out.println(searched);/////////////////////////////////////////////////////
+					return pathFinder(searched);
+			}
+		}
+		
+		System.out.println(desiredPath.peek());
+		desiredPath.pop();
+		return pathFinder(desiredPath.peek());
 	}
 
 }
--- src/Graph/RouteFinder.java
+++ src/Graph/RouteFinder.java
@@ -4,21 +4,65 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.Stack;
 
 
 public class RouteFinder {
-private Queue<String> correctDirection = new PriorityQueue();
+/*private Stack<String> desiredPath = new Stack<String>();
+GraphSolver gs = new GraphSolver();
 
+public Stack<String> pathFinder(String direction){
+	Queue<String> path = new PriorityQueue(); // these will store the looked at strings and return them to the queue on the map.
+	char[] currentPath = direction.toCharArray();
+	char[] comparePath = gs.paths.get(currentPath[1]).peek().toCharArray();
 
-private void pathFinder(Map<Character, Queue<String>> path){
-	Queue<String> hold = new PriorityQueue(); // these will store the looked at strings and return them to the queue on the map.
 	
+	if (currentPath[0] == 'i'){
+		return desiredPath;
+	}
+	
+	if (desiredPath.contains(direction)){
+		desiredPath.pop();
+		
+		path = gs.paths.get(currentPath[0]);
+		path.remove();
+		
+		gs.paths.put(currentPath[0], new PriorityQueue());
+		gs.paths.put(currentPath[0], path);
+		
+		return pathFinder(desiredPath.peek());
+	}
+	
+	if( gs.paths.get(currentPath[1]).isEmpty()){
+		desiredPath.pop();
+		return (pathFinder(desiredPath.peek()));
+	}
+	
+	if( currentPath[3] == comparePath[3] || currentPath[4] == comparePath[4] ){
+		desiredPath.push(direction);
+		return pathFinder(gs.paths.get(currentPath[0]).remove());
+	}
+	
+	if( currentPath[3] != comparePath[3] && currentPath[4] != comparePath[4] ){
+		int fullCheck = 0;
+		int full = gs.paths.get(currentPath[1]).size();
+		
+		for (String searched : gs.paths.get(currentPath[1])){
+			fullCheck++;
+			if(currentPath[3] == comparePath[3] || currentPath[4] == comparePath[4]){
+				return pathFinder(searched);
+			}
+		}
+	}
+	
+	desiredPath.pop();
+	return pathFinder(desiredPath.peek());
 }
 
 
 
 	
-	
+	*/
 		
 		
 	}
